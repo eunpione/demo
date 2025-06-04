ad0db7dbbad024bab949af7ea71c01ed26a2942b
Eungyeong Lee <eunpione22@gmail.com>
2025-06-04 17:32:22 +0900

    250604 cors error 해결 중

---
+++ spring_demo/src/main/java/com/spring/board/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.spring.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(true); // 쿠키/세션 허용 여부
+    }
+}
--- spring_demo/src/main/java/com/spring/board/controller/BoardController.java
+++ spring_demo/src/main/java/com/spring/board/controller/BoardController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/eun")
+@RequestMapping("/api")
 public class BoardController {
 
     //private final BoardService boardService;
